ab43a3c115bb35ffa8dfac6a974700afb99927b1
Alexander Hanke <alexander.hanke@ntnu.no>
2026-02-16 14:43:48 +0100

    Removed baselayer and box_bl

--- src/logic.cpp
+++ src/logic.cpp
@@ -19,7 +19,8 @@ along with this program; if not, see <http://www.gnu.org/licenses/>.
 --------------------------------------------------------------------
 Author: Hans Bihs
 --------------------------------------------------------------------*/
-#include"driver.h"
+
+#include"driver.h"
 #include"driver_headers.h"
 
 void driver::logic(lexer* p)
@@ -43,8 +44,6 @@ void driver::logic(lexer* p)
     pprint=new print_grid(p);
     
     pvtu = new print_vtu(p);
-
-    pbase = new baselayer(p,a);
 
     if(p->G10>0)
     pgeo = new geodat(p,a);
